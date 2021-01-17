8c8b7214cbfc25038fbcfd5aea54c00326d8d9b4
unknown <maazanwer64@gmail.com>
2021-01-17 09:07:28 -0800

    practice

---
+++ app.js
@@ -0,0 +1,406 @@
+// // CHAP 1
+// alert("Hello and welcome to my Page!")
+
+// alert("Error! Please enter a valid password.")
+
+// alert("Welcome to JS land... \n Happy Coding!!!"   )
+ 
+// alert("Welcome to JS Land...")
+// alert("Happy coding!!!")
+
+// var test = "hello there"
+// alert(test)
+// console.log(test)
+
+// alert("like i said in the browser!")
+
+
+
+// CHAP 2
+
+// var username;
+
+// var myName = "Hello my name is Maaz Anwer"
+
+// var message = "Hello World"
+// alert(message)
+
+// var name = "Jhone Doe"
+// var age = "15 years Old"
+// var aukaat = "Certified mobile application development"
+// alert(name);
+// alert(age);
+// alert(aukaat)
+
+// var pizza = "pizza \n  pizz \n piz \n pi \n p"
+// alert(pizza) 
+
+// var email = "maazanwer64@gmail.com"
+// alert(email)
+
+// var book = "A smarter way to learn javascript!!!"
+// alert(book)
+
+// var brow = "yeah!! i can write HTML Content through JavaScript!!!"
+// document.write(brow)
+
+// var latin = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(latin)
+
+
+
+// CHAP 3
+
+// var age = "I am 100 years old!!"
+// alert(age)
+
+// var vs = "You have visited this web page i bet 1 time right?\n i knew it!"
+// alert(vs)
+
+// var birthyear = "My birth year is 2001";
+// document.write(birthyear);
+
+// var names = "ALI MUGHAL "
+// var product = " shirts "
+// var quantity = " 3 "
+// var full = names + "Ordered" + quantity + product + "On Amazon 2 dollar store";
+// document.write(full)
+ 
+
+
+// CHAP 4
+
+// var variable1(legal variable names)
+// variable2
+// variable3
+// variable4
+// variable5
+
+// var 123variable(illegal variable names)
+// var \\variable
+// var .variable
+// var ++variable
+// var varia;ble
+
+//  var test = "RULES FOR NAMING JS VARIABLES "
+//  var condition1 = "Variable names can only contain _ , $ , numbers(123) , alphabets FOR Example: _1st$ name \n";
+//  var condition2 = "Variables must begin with a letter , 5 or dollar  For Example: 1name , _name , name \n";
+//  var condition3 = "Variable names are case sensitive \n";
+//  var condition4 = "Variable names should not be JS keywords \n";
+
+//  document.write(test  + "<br/>" , condition1+ "<br/>"   , condition2 + "<br/>"  , condition3+ "<br/>"  , condition4+ "<br/>" );
+
+
+
+
+// CHAP 5
+
+// var numberuno = 1
+// var numberduo = 2
+// var total = numberduo + numberuno ;
+// document.write(total)
+
+
+//  var numberuno = 1
+//  var numberduo = 2
+//  var total = numberduo - numberuno ;
+//  var numberuno = 1
+//  var numberduo = 2
+//  var total = numberduo * numberuno ;
+//  var numberuno = 1
+//  var numberduo = 2
+//  var total = numberduo / numberuno ;
+//  document.write();
+
+// var number1 =" The value of this variable is " + 5
+// var no2 =   5
+// var no3 = no2 + 1
+// var no4 = no3  + 7
+// var no5 = no4 - 1
+// var no6 = no5 / 3 
+// var output = no6
+// document.write(output)
+
+// var ticket = 600;
+// var quantity = 5
+// var total = ticket * quantity;
+// var para = "The Total cost to buy 5 movie tickets is: "
+// document.write(para + total )
+
+// var no1 = 4 * 1
+// var no2 = 4 * 2
+// var no3 = 4 * 3 
+// var no4 = 4 * 4 
+// var no5 = 4 * 5
+// document.write(no1 + "<br/>" , no2 + "<br/>" , no3 + "<br/>" , no4 + "<br/>", no5 + "<br/>");
+
+// var f = 70
+// var C = 27
+// var celsius = (f - 32 ) * 5/9
+// var fahrenheit = (C * 9/5) + 32
+// var para = "celsius temperature to fahrenheit is "
+// var para2 = "fahrenheit to celsius temperature is "
+// document.write(para + celsius + "<br/>", para2 + fahrenheit  )
+
+// var pr1 = 600
+// var pr2 = 150
+// var qu1 = 3 
+// var qu2 = 10
+// var ship = 100
+// var pa1 = "Price of item 1 is: " + pr1
+// var pa2 = "quantity is: " + qu1
+// var pa3 = "price of item 2 is : " + pr2
+// var pa4 = "quantity is: " + qu2 
+// var totpar = "Your total shall be: " + total
+// var tot1 = pr1 * qu1
+// var tot2 = pr2 * qu2
+// var total = ship + tot1 + tot2;
+// document .write(pa1 + "<br/>", pa2 + "<br/>", pa3 + "<br/>",pa4 + "<br/>",totpar + "<br/>")
+
+// var tot = 1100
+// var obt = 555 
+// var percentage = ( obt / tot ) * 100 ;
+
+// var p1 ="your marks obtained: " + obt
+// var p2 ="total marks: " + tot
+// var p3 = "your percentage is: " + percentage
+// document.write(p1 + "<br/>", p2 + "<br/>",p3 + "<br/>")
+
+// var exchange = (10 * 104.80) + (25 * 28)
+// document.write("Your Currency is: " + exchange)
+
+// var numbering = (((96 + 5 ) * 10 ) / 2);
+// document.write("number is " + numbering)
+
+// var date = 2001
+// var nowdate = 2020
+// var age = date - nowdate
+// document.write("Your Age is " + age)
+
+// var r = 5
+// var pi = 3.142
+// var circum = 2 * pi * r
+// var area = pi  *( r * r);
+// var pi1 = "The circumgerence is " + circum
+// var pi2 = "The area is " + area
+// var r1 = "the radius is " + r 
+// document.write(pi1 + "<br/>", pi2 + "<br/>" , r1 )
+
+// var sn = 1
+// var age = 19 
+// var totalage = 20
+// var est = 2
+
+// var com = age - totalage
+// var com1 = com * 365
+// var com2 = com1 * est
+// document.write("You Will need" + com2 + "to last you to the death age of " + totalage);
+
+
+
+// CHAP 6
+
+// var para1 = "The value of a++ is "
+// var para2 = "The value of a-- is "
+// var para = "The value of a is: "
+// var n = 10;
+// var a = ++ n
+// var b = ++ a 
+// var c = -- b
+// var d = -- c
+// document.write(para + n + "<br/>", 
+//     para1 + a + "<br/>",
+//     para1 + b + "<br/>",
+//     para2 + c + "<br/>",
+//     para2 + d + "<br/>" )
+
+// var a = 2;
+// var b = 1;
+// var para = "The value is "
+// var z = --a
+// var x = --a - --b;
+// var c = --a - --b  + ++b;
+// var v = --a - --b + ++b + b--;
+// document.write(para+ z + "<br/>" , para + x + "<br/>",para + c + "<br/>",para + v + "<br/>")
+
+// var name = prompt("hello what is your name?");
+// document.write("Hello " + name + " How are you");
+
+// var number = prompt("Tell us your number!!")
+// for (var i = 1; i <= 10; i++){
+//     // var result = number * i + "<br/>"
+// document.write (number +  "X" +  i + "==" + result)
+// }
+
+
+
+// CHAP 9
+
+// var x = prompt("Where are you from??")
+// if(x === "karachi"){
+//     alert("City of lights")
+// }
+
+// var x = prompt("Your gender??")
+// if(x === "male"){
+//     alert("GOOD MORNING SIR")
+// }else if(x === "female"){
+//     alert("GOOD MORNING MA'AM")
+// }
+
+// var x = prompt("signal general knowledge")
+// if(x === "red"){
+//     alert("STOP!!!")
+// }
+// else if(x === "yellow"){
+//     alert("Ready to move")
+// }else if(x === "green"){
+//     alert("GO!!");
+// }
+
+// var x = prompt("PLEASE ENTER YOUR FUEL AMOUNT IN LITRES")
+// if(x < "0.25"){
+//     alert("PLEASE REFILL THE FUEL IN YOUR CAR")
+// }else if(x => "0.25"){
+//     alert("You are good to go")}
+
+// var  a = 4
+// if(++a === 5){
+//     alert("given condtion for variable is true")
+// // } [YES]
+// var b = 82
+// if(++b === 83){
+//     alert("given condtion for variable is true")
+// }[YES]
+// var c = 12
+// if(++c === 13){
+//     alert("given condtion1 for variable is true")
+// }
+// var materialcost = 20000
+// var laborcost = 2000
+// var totalcost = materialcost + laborcost
+// if(totalcost === materialcost + laborcost){
+//     alert("the variable is true")
+// }
+// if( "car" < "cat"){
+//     alert("The cat is weirdly bigger")
+// }
+
+// var a = prompt("Guess the number(YES WITHOUT ANY HINTS!!)")
+// if(a === "19"){
+//     alert("Bingo!! You guessed it correct(QMOBILE APKA HOA!)")
+// }
+// else if(a === (19 + 1) ){
+//     alert("You are really close!!")
+// }
+// else{
+//     alert("NOT EVEN CLOSE!!!")
+// }
+
+// var number = prompt("is your number divisible by three?");
+// if(number === 1 , 3 , 6 , 9){
+//    var result =  number / 3
+//     alert(result)
+// }
+// else{
+//     alert("no")
+// }
+
+// var temperature = prompt("What is your city's temperature?")
+// if(temperature > 40 && temperature < 50 ){
+//     alert("its too hot")
+// }if(temperature > 30 && temperature < 40){
+//     alert("The temperature is normal")
+// }if(temperature > 20 && temperature < 30){
+//     alert("Todays weather is cold")
+// }if(temperature > 10 && temperature < 20){
+//     alert("OMG!! ITS TOO COLD")
+// // }
+
+
+
+
+// CHAP 11-12
+
+// var a = prompt("Enter an integer")
+// var b = prompt("Enter another integer")
+// if(a > b ){
+//     alert("A wins" +  a)
+// }else if(b > a){
+//     alert("B wins " +  b)
+// }
+
+// var a = prompt("Enter an integer")
+// if(a === "-"){
+//     alert("Negative is an imaginary number")
+// }
+// else if(a === 0){
+//     alert("ZERO WHY???!!")
+// }
+// else{
+//     alert("A positive integer daring arent we??")
+// }
+    
+// var n = prompt("Enter a variable")
+// if(n === "a" || n === "e" || n ===  "o" || n === "u" || n === "i")  {
+//     alert("A vowel huh?")
+// }
+// else{
+//     alert("No vowel detected!")
+// }    
+
+// var x = 13
+// if( x < 18){
+//     alert("Good day")
+// }
+// else if(x > 18 && x <= 24){
+//     alert("Good evening")
+// }
+// else{
+//     alert("You know we are talking about time right!!!")}
+ 
+
+// var a = prompt("Enter your time in 24 hours format thank you!!!!");    
+// if(a > 0000 && a < 1200){
+//     alert("Good morning")
+    
+// }else if(a > 1100 && a < 1700){
+//     alert("GOOD AFTERNOON")
+    
+// }else if(a > 1600 && a < 2100){
+//     alert("GOOD EVENING")
+
+// }else if(a > 2000 && a < 2400){
+//     alert("GOoD NIGHT!!")
+    
+// }else{
+//     alert("Nope time is till 2400")
+// }
+
+
+
+// CHAP 14
+
+// var ary = [];
+
+// var ary = ["a" , "b" , "c"];
+// document.write(ary)
+
+// var ary = [4 , 5 , 6]
+// document.write(ary)
+
+// var ary =  [ 1 , "a" , 47 , 48 , "How are yuh?" , true , "Well how the table turns!!"]
+// document.write(ary)
+
+// var q = "QUALIFICATIONS: "
+// document.write(q + "<br/>")
+
+// var arr = ["SSC" , "HSC" , "FSC" , "BSC" , "BS" , "MD" , "MCOM" , "BSCS" , "BSSE"]
+// for (var i = 0; i <arr.length ; i++ ){
+//     document.write("&bull;" + arr[i] + "<br/>")
+// }
+
+// var arr1 = ["Michael's ", "SOHN's ", "JOHN's "];
+// var arr2 = ["36%","78%","89"];
+// var p = "percentage is"
+// document.write(arr1[0] + p + arr2[0] + "<br/>", arr1[1] + p + arr2[1] + "<br/>", arr1[2] + p + arr2[2] + "<br/>");
